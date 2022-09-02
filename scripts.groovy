443dfd87c3aef8c925e779dc7c95e7bc749613a5
Micah Shallom <micahshallom@gmail.com>
2022-09-02 18:45:13 +0100

    fix

--- scripts.groovy
+++ scripts.groovy
@@ -66,7 +66,7 @@ def pushImageToHub(){
 def deployInfrastructure(){
     sh """
         wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-        echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+        echo 'deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main' | sudo tee /etc/apt/sources.list.d/hashicorp.list
         sudo apt update && sudo apt install terraform
     """
     sh "make init"
